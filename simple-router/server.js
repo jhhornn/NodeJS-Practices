81bc72f5241e3cfb474dd66715519320deb299ed
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-04 20:05:26 +0100

    updated server-router

--- simple-router/server.js
+++ simple-router/server.js
@@ -6,25 +6,43 @@ const PORT = 4000
 const HOST_NAME = 'localhost';
 
 const requestHandler = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    console.log(req.url)
-    console.log(req.method)
-    
-    switch(req.url) {
-        case '/books': 
-            res.end(JSON.stringify(books));
-            break;
-        case '/authors':
-            res.end(JSON.stringify(authors));
-            break;
-        default:
-            res.writeHead(404);
-            res.end(JSON.stringify({
-                message: 'Not Found'
-            }));
-    }
+	res.setHeader(
+		"Content-Type",
+		"application/json",
+	);
+	console.log(req.url);
+	console.log(req.method);
 
+	//Using the switch statement
+	// switch(req.url) {
+	//     case '/books':
+	//         res.end(JSON.stringify(books));
+	//         break;
+	//     case '/authors':
+	//         res.end(JSON.stringify(authors));
+	//         break;
+	//     default:
+	//         res.writeHead(404);
+	//         res.end(JSON.stringify({
+	//             message: 'Not Found'
+	//         }));
+	// }
+
+	//Using if-else
+	if (req.url == "/books") {
+		res.end(JSON.stringify(books));
+	} else if (req.url == "/authors") {
+		res.end(JSON.stringify(authors));
+	} else {
+		res.writeHead(404);
+		res.end(
+			JSON.stringify({
+				message: "Not Found",
+			}),
+		);
+	}
 }
+
 
 const server = http.createServer(requestHandler)
 
