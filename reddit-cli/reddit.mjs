d862bb9b647d834ac51d82600ae42673e85d3608
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-21 15:31:49 +0100

    completed reddit cli

--- reddit-cli/reddit.mjs
+++ reddit-cli/reddit.mjs
@@ -1,4 +1,23 @@
 #! /usr/bin/env node
-
 // The above is done to make sure it runs on node
-console.log('Welcome to reddit')
+
+import fetch from 'node-fetch';
+import open from 'open';
+import yargs from 'yargs';
+
+const { argv } = yargs(process.argv);
+
+const res = await fetch('https://reddit.com/.json');
+const data = await res.json();
+const children = data.data.children
+const randomPost = children[Math.floor(Math.random() * children.length)]
+const link = `https://reddit.com${randomPost.data.permalink}`
+
+if (argv.print) {
+    console.log(`
+        title: ${randomPost.title}
+        link: ${link}
+    `);
+} else {
+    open(link);
+}
