37f11733c1c345d2f1ddfdd99377e71d491e2b91
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-22 14:56:12 +0100

    asynchronous testing

--- express/lib/utils.spec.js
+++ express/lib/utils.spec.js
@@ -15,3 +15,23 @@ describe('mapObjectToArray()', () => {
         expect(mockCb.mock.calls.length).toBe(2)
     })
 })
+
+describe('getNewUser', () => {
+    test('it gets user', async() => {
+        const user = await getNewUser(1)
+
+        expect(user).toBeTruthy();
+        expect(user.id).toBe(1)
+    })
+
+    test('no user found', async() => {
+        expect.assertions(1) //One assertion must be called
+        //which only happens when the try block fails or no user is found.
+
+        try {
+            const user = await getNewUser(30)
+        } catch(e) {
+            expect(e).toBeTruthy();
+        }
+    })
+})
